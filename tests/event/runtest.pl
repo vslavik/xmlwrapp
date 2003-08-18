1b74a0cb7523c695578e9e6ffa5d430d65e36a9e
Peter J Jones <pjones@pmade.org>
2003-08-18 04:33:53 +0000

    make better use of testtools/harness.pm

--- tests/event/runtest.pl
+++ tests/event/runtest.pl
@@ -39,86 +39,17 @@ my $test = harness->new("xml::event_parser");
 runtests();
 
 ###########################################################################
-sub runtests {
-    my $actual_result;
-    my $good_result;
-
+sub runtests
+{
     ###########################################################################
-    $test->start("test_event-01");
-    $actual_result = `./test_event-01 data/01.xml 2>&1`;
-
-    if ($? != 0) {
-	$test->fail("test process returned $?");
-    } else {
-	$good_result = slurp_file("data/01.out");
-
-	if ($actual_result ne $good_result) {
-	    $test->fail('output did not match expected value');
-	} else {
-	    $test->pass();
-	}
-    }
+    $test->regression("callbacks (01)", "./test_event-01 data/01.xml", "data/01.out");
     ###########################################################################
     foreach my $cbname (qw(start_element end_element text processing_instruction comment cdata)) {
-	$test->start("test_event-02 ($cbname)");
-	$actual_result = `./test_event-02 data/01.xml $cbname 2>&1`;
-
-	if ($? != 0) {
-	    $test->fail("test process returned $?");
-	} else {
-	    chomp($actual_result);
-	    
-	    if ($actual_result ne $cbname) {
-		$test->fail("expected to see $cbname, but saw $actual_result instead");
-	    } else {
-		$test->pass();
-	    }
-	}
-
-	$test->start("test_event-02 ($cbname throw)");
-	$actual_result = `./test_event-02 data/01.xml $cbname throw 2>&1`;
-
-	if ($? != 0) {
-	    $test->fail("test process returned $?");
-	} else {
-	    chomp($actual_result);
-
-	    if ($actual_result ne "$cbname throw") {
-		$test->fail("expected to see '$cbname throw', be saw '$actual_result' instead");
-	    } else {
-		$test->pass();
-	    }
-	}
+	$test->regression("$cbname (02)", "./test_event-02 data/01.xml $cbname", "$cbname\n");
+	$test->regression("$cbname-throw (02)", "./test_event-02 data/01.xml $cbname throw", "$cbname throw\n");
     }
     ###########################################################################
-    $test->start("test_event-03");
-    $actual_result = `./test_event-03 data/01.xml 2>&1`;
-
-    if ($? != 0) {
-	$test->fail("test process retured $?");
-    } else {
-	if ($actual_result =~ /inside two_dot_two/) {
-	    $test->pass();
-	} else {
-	    $test->fail("expected to see 'inside two_dot_two' in result");
-	}
-    }
+    $test->regression("cdata2text (03)", "./test_event-03 data/01.xml", qr/inside two_dot_two/);
     ###########################################################################
-}
-###########################################################################
-sub slurp_file {
-    my $filename = shift;
-    my $out;
-
-
-    if (not open(SF, $filename)) {
-	print STDERR "\n\n$0: failed to open $filename: $!\n";
-	exit 1;
-    }
-
-    my $save = $/; $/=undef; $out = <SF>; $/=$save;
-    close SF;
-
-    return $out;
 }
 ###########################################################################
