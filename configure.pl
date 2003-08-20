e490c4b6fe4700bb21932518e74969673ee1ff03
Peter J Jones <pjones@pmade.org>
2003-08-20 03:56:19 +0000

    fix a build problem on Linux (bug #10)

--- configure.pl
+++ configure.pl
@@ -48,7 +48,7 @@ use Cwd qw(cwd chdir);
 #
 ################################################################################
 use constant DATE		=> 'Tue Jan 15 08:56:06 2002';
-use constant ID			=> '$Id: configure.pl,v 1.1.1.1 2003-08-04 04:57:07 pjones Exp $';
+use constant ID			=> '$Id: configure.pl,v 1.1.1.1.4.1 2003-08-20 03:56:19 pjones Exp $';
 ################################################################################
 #
 # Global Variables
@@ -280,12 +280,15 @@ sub load_flags_from_xml_config {
     my $main_include_dir = undef;
     my $xslt_include_dir = undef;
 
+    # add a few standard directories just in case
+    push(@include_dirs, '/usr/include');
+
     foreach my $dir (@include_dirs) {
-	if (-d "$dir${dirsep}libxml") {
+	if (-d "$dir${dirsep}libxml" and not defined $main_include_dir) {
 	    $main_include_dir = $dir;
 	}
 
-	if (-e "$dir${dirsep}libxslt${dirsep}transform.h") {
+	if (-e "$dir${dirsep}libxslt${dirsep}transform.h" and not defined $xslt_include_dir) {
 	    $xslt_include_dir = $dir;
 	}
 
