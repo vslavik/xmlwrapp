fbe1b7a509120eb67fb17439e776915c43f6a6c6
Vaclav Slavik <vslavik@gmail.com>
2010-04-10 08:39:38 +0000

    Fix error handling in tree_parser without exceptions.
    If libxml2 returned NULL document without errors (e.g. because a missing
    file is just a warning in it), pimpl_->okay_ was left as true. This was
    not only clearly wrong, but also inconsistent with exceptions handling:
    if exceptions were allowed, an exception _would_ be thrown in this
    situation.

--- src/libxml/tree_parser.cxx
+++ src/libxml/tree_parser.cxx
@@ -160,6 +160,8 @@ tree_parser::tree_parser(const char *name, bool allow_exceptions)
         if (tmpdoc)
             xmlFreeDoc(tmpdoc);
 
+        pimpl_->okay_ = false;
+
         if (allow_exceptions)
             throw xml::exception(pimpl_->last_error_);
     }
@@ -192,6 +194,7 @@ tree_parser::tree_parser(const char *data, size_type size, bool allow_exceptions
         ctxt->myDoc = 0;
         ctxt->sax = 0;
         xmlFreeParserCtxt(ctxt);
+
         pimpl_->okay_ = false;
 
         if (allow_exceptions)
