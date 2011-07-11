c057a87852d45f2e9b93b06659e6f16d51dfe6d1
Vaclav Slavik <vslavik@gmail.com>
2011-07-11 14:22:16 +0200

    Update email addresses.

--- src/libxml/node.cxx
+++ src/libxml/node.cxx
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2001-2003 Peter J Jones (pjones@pmade.org)
- *               2009      Vaclav Slavik <vslavik@fastmail.fm>
+ *               2009      Vaclav Slavik <vslavik@gmail.com>
  * All Rights Reserved
  * 
  * Redistribution and use in source and binary forms, with or without
