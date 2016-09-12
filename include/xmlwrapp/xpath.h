81100faa11273018954725dc5f61f7cab987995c
Vadim Zeitlin <vadim@zeitlins.org>
2016-09-12 19:30:57 +0200

    Throw exception when evaluating an invalid XPath expression
    Propagate libxslt and libxml2 error messages to the application as exceptions
    by default instead of simply ignoring them.

--- include/xmlwrapp/xpath.h
+++ include/xmlwrapp/xpath.h
@@ -43,6 +43,7 @@
 
 // xmlwrapp includes
 #include "xmlwrapp/init.h"
+#include "xmlwrapp/errors.h"
 #include "xmlwrapp/export.h"
 #include "xmlwrapp/nodes_view.h"
 
@@ -105,17 +106,24 @@ public:
         xml::node& argument and pass xml::document::get_root_node() result to
         it.
      */
-    const_nodes_view evaluate(const std::string& expr);
+    const_nodes_view evaluate(const std::string& expr,
+                              error_handler& on_error = throw_on_error);
 
     /**
         Execute an XPath query in the scope of XML node @a n.
 
         @param  expr  XPath expression.
         @param  n     The context node for the expression.
+        @param  on_error Error handler throwing an exception if an error occurs
+            during the expression evaluation. Notice that absence of matches
+            isn't considered to be an error, in this case the function just
+            returns an empty set.
 
         @return Const set of matching nodes.
      */
-    const_nodes_view evaluate(const std::string& expr, const xml::node& n);
+    const_nodes_view evaluate(const std::string& expr,
+                              const xml::node& n,
+                              error_handler& on_error = throw_on_error);
 
     /**
         Execute an XPath query in the scope of XML node @a n.
@@ -123,7 +131,9 @@ public:
         This overload is identical to the one taking const @a n argument,
         except that it returns a set of nodes that can be modified.
      */
-    nodes_view evaluate(const std::string& expr, xml::node& n);
+    nodes_view evaluate(const std::string& expr,
+                        xml::node& n,
+                        error_handler& on_error = throw_on_error);
 
 private:
     // no copying
