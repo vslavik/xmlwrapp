85e4754f7292b7fb8bbdd8b3ec20ebc364405804
Vadim Zeitlin <vz-xmlwrapp@zeitlins.org>
2018-04-08 17:09:50 +0200

    Increase version to 0.9.0
    Prepare for 0.9.0 release: adding RelaxNG support arguably justifies
    updating the version number.

--- include/xmlwrapp/version.h
+++ include/xmlwrapp/version.h
@@ -40,8 +40,8 @@
 #define _xmlwrapp_version_h_
 
 #define XMLWRAPP_VERSION_MAJOR   0
-#define XMLWRAPP_VERSION_MINOR   8
-#define XMLWRAPP_VERSION_MICRO   1
+#define XMLWRAPP_VERSION_MINOR   9
+#define XMLWRAPP_VERSION_MICRO   0
 
 /**
     Checks if xmlwrapp version is at least @a major.@a minor.@a micro.
