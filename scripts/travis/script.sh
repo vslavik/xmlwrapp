082b439e2d577a78495c76753ae214a68f0314a6
Vadim Zeitlin <vadim@zeitlins.org>
2020-10-11 16:49:23 +0200

    Run tests using Wine in Travis CI builds cross-compiling for MSW
    Run unit tests too to show that the MSW builds actually work and not
    just compile.

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -16,8 +16,15 @@ fi
 make
 
 if [ -n "$HOST" ]; then
-    # TODO: Use Wine.
-    echo "*** Skipping tests for cross-builds."
-else
-    make check
+    # As long as we don't have any other tests (e.g. written in interpreted
+    # language), just setting LOG_COMPILER to Wine is good enough. If we do
+    # need to run e.g. Python tests too, we would need a script selecting the
+    # right way to run the test depending on its extension as, unfortunately,
+    # setting EXE_LOG_COMPILER doesn't work (although PY_LOG_COMPILER does).
+    export LOG_COMPILER=wine-development
+
+    # Set up the path to allow the tests to find libstdc++ and libxml2 DLLs.
+    export WINEPATH="$(dirname $($HOST-g++ -print-libgcc-file-name));/usr/local/bin"
 fi
+
+make check
