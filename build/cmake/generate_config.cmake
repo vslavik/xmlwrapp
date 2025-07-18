50ab79df4eeeae71baccf1dfd31e0834ec259734
Vadim Zeitlin <vadim@zeitlins.org>
2025-07-18 05:25:25 +0200

    Improve config files installation
    Don't overwrite existing files if they don't change and give the same
    message for them as for all the other files.

--- build/cmake/generate_config.cmake
+++ build/cmake/generate_config.cmake
@@ -35,24 +35,64 @@ endif( XMLWRAPP_WITH_LIBXSLT )
 
 string(STRIP ${LEGACY_LINK_FLAGS} LEGACY_LINK_FLAGS)
 
-configure_file(
+# Function to configure a file if it is necessary, i.e. if it doesn't exist or
+# its contents has changed.
+#
+# This outputs the same messages as CMake install(), making the installation of
+# the config files here consistent with all the other ones.
+function(configure_file_if_necessary input_file output_file make_executable)
+  # Create temporary file
+  set(temp_file "${output_file}.tmp")
+  set(configure_args "@ONLY")
+  if(make_executable)
+      list(APPEND configure_args "FILE_PERMISSIONS")
+      list(APPEND configure_args "OWNER_EXECUTE")
+      list(APPEND configure_args "OWNER_WRITE")
+      list(APPEND configure_args "OWNER_READ")
+      list(APPEND configure_args "GROUP_EXECUTE")
+      list(APPEND configure_args "GROUP_READ")
+      list(APPEND configure_args "WORLD_EXECUTE")
+      list(APPEND configure_args "WORLD_READ")
+  endif()
+  configure_file("${input_file}" "${temp_file}" ${configure_args})
+
+  # Check if we need to update
+  set(needs_update TRUE)
+
+  if(EXISTS "${output_file}")
+      file(READ "${output_file}" existing_content)
+      file(READ "${temp_file}" new_content)
+
+      if("${existing_content}" STREQUAL "${new_content}")
+          set(needs_update FALSE)
+          message(STATUS "Up-to-date: ${output_file}")
+      endif()
+  endif()
+
+  if(needs_update)
+      message(STATUS "Installing: ${output_file}")
+      file(RENAME "${temp_file}" "${output_file}")
+  else()
+      file(REMOVE "${temp_file}")
+  endif()
+endfunction()
+
+configure_file_if_necessary(
   ${PROJECT_SOURCE_DIR}/xmlwrapp.pc.in
   ${CMAKE_INSTALL_FULL_LIBDIR}/pkgconfig/xmlwrapp.pc
-  @ONLY
+  OFF
 )
 
-configure_file(
+configure_file_if_necessary(
   ${PROJECT_SOURCE_DIR}/xmlwrapp-config.in
   ${CMAKE_INSTALL_FULL_BINDIR}/xmlwrapp-config
-  @ONLY
-  FILE_PERMISSIONS
-    OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
+  ON
 )
 
 if( XMLWRAPP_WITH_LIBXSLT )
-  configure_file(
+  configure_file_if_necessary(
     ${PROJECT_SOURCE_DIR}/xsltwrapp.pc.in
     ${CMAKE_INSTALL_FULL_LIBDIR}/pkgconfig/xsltwrapp.pc
-    @ONLY
+    OFF
   )
 endif( XMLWRAPP_WITH_LIBXSLT )
